1b29436e5e3385c5e2db8f552ddaed79d1515d3a
Nyayic <chrosbiefannie@gmail.com>
2022-06-22 11:34:21 +0300

    Modified about, carousel sections and

---
+++ web/sites/default/files/php/twig/62b2c9585d437_views-view--team--teamblo_-y9m_01tDTyTddmj-cMrugk9G/VthJWfbMSXzEyrJFMliso6ZyzSjOu0INalFwonfgygg.php
@@ -0,0 +1,324 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/klean02/templates/views/views-view--team--teamblock.html.twig */
+class __TwigTemplate_22aff7219f9e1b7e18ab48ff11e7c0a9e11932e44530a3ebfb06f68c4120434b extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 34, "if" => 44];
+        $filters = ["clean_class" => 36, "escape" => 43];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 34
+        $context["classes"] = [0 => "view", 1 => ("view-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 36
+($context["id"] ?? null)))), 2 => ("view-id-" . $this->sandbox->ensureToStringAllowed(        // line 37
+($context["id"] ?? null))), 3 => ("view-display-id-" . $this->sandbox->ensureToStringAllowed(        // line 38
+($context["display_id"] ?? null))), 4 => ((        // line 39
+($context["dom_id"] ?? null)) ? (("js-view-dom-id-" . $this->sandbox->ensureToStringAllowed(($context["dom_id"] ?? null)))) : (""))];
+        // line 42
+        echo "
+  ";
+        // line 43
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null)), "html", null, true);
+        echo "
+  ";
+        // line 44
+        if (($context["title"] ?? null)) {
+            // line 45
+            echo "    ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+            echo "
+  ";
+        }
+        // line 47
+        echo "  ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null)), "html", null, true);
+        echo "
+  ";
+        // line 48
+        if (($context["header"] ?? null)) {
+            // line 49
+            echo "    <div class=\"view-header\">
+      ";
+            // line 50
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["header"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 53
+        echo "  ";
+        if (($context["exposed"] ?? null)) {
+            // line 54
+            echo "    <div class=\"view-filters\">
+      ";
+            // line 55
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["exposed"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 58
+        echo "  ";
+        if (($context["attachment_before"] ?? null)) {
+            // line 59
+            echo "    <div class=\"attachment attachment-before\">
+      ";
+            // line 60
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attachment_before"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 63
+        echo "
+  ";
+        // line 64
+        if (($context["rows"] ?? null)) {
+            // line 65
+            echo "  <div class=\"row\">
+    <div class=\"owl-carousel team-carousel position-relative\">
+      ";
+            // line 67
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["rows"] ?? null)), "html", null, true);
+            echo " 
+    </div>
+  ";
+        } elseif (        // line 69
+($context["empty"] ?? null)) {
+            // line 70
+            echo "    <div class=\"view-empty\">
+      ";
+            // line 71
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["empty"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 74
+        echo "
+  ";
+        // line 75
+        if (($context["pager"] ?? null)) {
+            // line 76
+            echo "    ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["pager"] ?? null)), "html", null, true);
+            echo "
+  ";
+        }
+        // line 78
+        echo "  ";
+        if (($context["attachment_after"] ?? null)) {
+            // line 79
+            echo "    <div class=\"attachment attachment-after\">
+      ";
+            // line 80
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attachment_after"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 83
+        echo "  ";
+        if (($context["more"] ?? null)) {
+            // line 84
+            echo "    ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["more"] ?? null)), "html", null, true);
+            echo "
+  ";
+        }
+        // line 86
+        echo "  ";
+        if (($context["footer"] ?? null)) {
+            // line 87
+            echo "    <div class=\"view-footer\">
+      ";
+            // line 88
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["footer"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 91
+        echo "  ";
+        if (($context["feed_icons"] ?? null)) {
+            // line 92
+            echo "    <div class=\"feed-icons\">
+      ";
+            // line 93
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["feed_icons"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 96
+        echo "  </div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/klean02/templates/views/views-view--team--teamblock.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  197 => 96,  191 => 93,  188 => 92,  185 => 91,  179 => 88,  176 => 87,  173 => 86,  167 => 84,  164 => 83,  158 => 80,  155 => 79,  152 => 78,  146 => 76,  144 => 75,  141 => 74,  135 => 71,  132 => 70,  130 => 69,  125 => 67,  121 => 65,  119 => 64,  116 => 63,  110 => 60,  107 => 59,  104 => 58,  98 => 55,  95 => 54,  92 => 53,  86 => 50,  83 => 49,  81 => 48,  76 => 47,  70 => 45,  68 => 44,  64 => 43,  61 => 42,  59 => 39,  58 => 38,  57 => 37,  56 => 36,  55 => 34,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override for a main view template.
+ *
+ * Available variables:
+ * - attributes: Remaining HTML attributes for the element.
+ * - css_name: A css-safe version of the view name.
+ * - css_class: The user-specified classes names, if any.
+ * - header: The optional header.
+ * - footer: The optional footer.
+ * - rows: The results of the view query, if any.
+ * - empty: The content to display if there are no rows.
+ * - pager: The optional pager next/prev links to display.
+ * - exposed: Exposed widget form/info to display.
+ * - feed_icons: Optional feed icons to display.
+ * - more: An optional link to the next page of results.
+ * - title: Title of the view, only used when displaying in the admin preview.
+ * - title_prefix: Additional output populated by modules, intended to be
+ *   displayed in front of the view title.
+ * - title_suffix: Additional output populated by modules, intended to be
+ *   displayed after the view title.
+ * - attachment_before: An optional attachment view to be displayed before the
+ *   view content.
+ * - attachment_after: An optional attachment view to be displayed after the
+ *   view content.
+ * - dom_id: Unique id for every view being printed to give unique class for
+ *   Javascript.
+ *
+ * @see template_preprocess_views_view()
+ */
+#}
+{%
+  set classes = [
+    'view',
+    'view-' ~ id|clean_class,
+    'view-id-' ~ id,
+    'view-display-id-' ~ display_id,
+    dom_id ? 'js-view-dom-id-' ~ dom_id,
+  ]
+%}
+
+  {{ title_prefix }}
+  {% if title %}
+    {{ title }}
+  {% endif %}
+  {{ title_suffix }}
+  {% if header %}
+    <div class=\"view-header\">
+      {{ header }}
+    </div>
+  {% endif %}
+  {% if exposed %}
+    <div class=\"view-filters\">
+      {{ exposed }}
+    </div>
+  {% endif %}
+  {% if attachment_before %}
+    <div class=\"attachment attachment-before\">
+      {{ attachment_before }}
+    </div>
+  {% endif %}
+
+  {% if rows %}
+  <div class=\"row\">
+    <div class=\"owl-carousel team-carousel position-relative\">
+      {{ rows }} 
+    </div>
+  {% elseif empty %}
+    <div class=\"view-empty\">
+      {{ empty }}
+    </div>
+  {% endif %}
+
+  {% if pager %}
+    {{ pager }}
+  {% endif %}
+  {% if attachment_after %}
+    <div class=\"attachment attachment-after\">
+      {{ attachment_after }}
+    </div>
+  {% endif %}
+  {% if more %}
+    {{ more }}
+  {% endif %}
+  {% if footer %}
+    <div class=\"view-footer\">
+      {{ footer }}
+    </div>
+  {% endif %}
+  {% if feed_icons %}
+    <div class=\"feed-icons\">
+      {{ feed_icons }}
+    </div>
+  {% endif %}
+  </div>
+", "themes/contrib/klean02/templates/views/views-view--team--teamblock.html.twig", "C:\\xampp\\htdocs\\klean02\\web\\themes\\contrib\\klean02\\templates\\views\\views-view--team--teamblock.html.twig");
+    }
+}
